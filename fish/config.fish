219652a60e9e60a9f22ea76ae870e1187e90acc4
Shaun Patterson <shaunpatterson@gmail.com>
2021-02-09 13:34:32 -0500

    New settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
+#set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 pyenv init - | source
-
 set -Ux PYENV_VERSION 3.8.1
 
-echo "TEST"
+set -U fish_user_paths ~/go/bin $fish_user_paths
